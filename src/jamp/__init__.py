12c3b5f57b7ddf6d8541c9bf3c6b43c1b1f79522
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2026-08-09 14:17:44 +0200

    Add --version arg, fix STDHDRS, bump to v0.7.8

--- src/jamp/__init__.py
+++ src/jamp/__init__.py
@@ -1,2 +1,15 @@
+from importlib.metadata import PackageNotFoundError, version
+
+
+def get_version():
+    """Return installed distribution version, or unknown in an uninstalled checkout."""
+    try:
+        return version("jam-build")
+    except PackageNotFoundError:
+        return "unknown"
+
+
+__version__ = get_version()
+
 # make this callable as script
 from jamp.build import main_cli as main_cli
